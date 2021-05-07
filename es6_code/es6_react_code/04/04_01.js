41aac49dcc1f656224912f1c0099053e70e78596
姚观寿 <yaoguanshou@rainbowcn.com>
2021-05-07 10:03:18 +0800

    更新react源码分析

--- es6_code/es6_react_code/04/04_01.js
+++ es6_code/es6_react_code/04/04_01.js
@@ -1,4 +1,11 @@
-<<<<<<< HEAD
+/*
+ * @Author: your name
+ * @Date: 2021-05-06 12:29:53
+ * @LastEditTime: 2021-05-07 10:02:10
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /react/es6_code/es6_react_code/04/04_01.js
+ */
 // import React, { useState, useEffect } from 'react'
 
 const Show =(props)=>{
@@ -38,44 +45,3 @@ const LazyPage = () => {
 };
 
 ReactDOM.render(<LazyPage />, document.getElementById("example"));
-=======
-// import React, { useState, useEffect } from 'react'
-
-const Show =(props)=>{
-  const {flag}=props;
-  React.useEffect(() => {
-     console.log(1)
-
-    return () => {
-      console.log(2)
-
-    };
-  });
-
-  return (
-    <div> {flag?(<div> 显示</div> ):null} </div>
-  )
-
-}
-
-const LazyPage = () => {
-  const [name, setName] = React.useState("zhangsan");
-  const [flag, setFlag] = React.useState(true);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setFlag(false);
-        }}
-      >
-        按钮
-      </button>
-      <Show flag={flag}> </Show>
-      <p> My Name is: {name} </p>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
-    </div>
-  );
-};
-
-ReactDOM.render(<LazyPage />, document.getElementById("example"));
->>>>>>> 46e5d654750168326619cd5ec4cf48a1c0506d86
